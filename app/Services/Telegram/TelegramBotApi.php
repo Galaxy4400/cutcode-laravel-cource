e5a6c386033aefb0bb18c5224c60db2400830887
Moiseev Evgeny <moiseevEO@yandex.ru>
2023-10-12 22:57:59 +0300

    Try cache

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,17 +2,27 @@
 
 namespace App\Services\Telegram;
 
+use Throwable;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotApi
 {
 	public const HOST = 'https://api.telegram.org/bot';
 
-	public static function sendMessage(string $token, int $chatId, string $text): void
+	public static function sendMessage(string $token, int $chatId, string $text): bool
 	{
-		Http::get(self::HOST . $token . '/sendMessage', [
-			'chat_id' => $chatId,
-			'text' => $text,
-		]);
+		try {
+			Http::get(self::HOST . $token . '/sendMessage', [
+				'chat_id' => $chatId,
+				'text' => $text,
+			]);
+
+			return true;
+
+		} catch (Throwable $e) {
+				report($e);
+
+				return false;
+		}
 	}
 }
