42eb28eba7ce8a36c76305c83082b5f86f86413d
Moiseev Evgeny <moiseevEO@yandex.ru>
2023-10-17 17:07:46 +0300

    Stage 2 fixing complite

--- database/migrations/2023_10_16_120204_create_products_table.php
+++ database/migrations/2023_10_16_120204_create_products_table.php
@@ -9,9 +9,7 @@ use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-	/**
-	 * Run the migrations.
-	 */
+
 	public function up(): void
 	{
 		Schema::create('products', function (Blueprint $table) {
@@ -31,9 +29,7 @@ return new class extends Migration
 		});
 	}
 
-	/**
-	 * Reverse the migrations.
-	 */
+
 	public function down(): void
 	{
 		if (app()->isLocal()) {
