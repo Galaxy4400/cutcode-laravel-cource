d3d44a307a5b4e1e5f31d6f3f78e412039460605
Moiseev Evgeny <moiseevEO@yandex.ru>
2023-10-19 15:34:25 +0300

    Unique slug complite

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -6,15 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasSlug
 {
+
 	protected static function bootHasSlug(): void
 	{
 		static::creating(function (Model $item) {
-			$item->slug = $item->slug ?? str($item->{self::slugFrom()})->append(time())->slug();
+			$item->slug = $item->slug ?? self::generateSlug($item);
 		});
 	}
+
+
+	protected static function generateSlug(Model $item, int $level = 0): string
+	{
+		$suffix = $level ? '-'.$level : '';
+
+		$slug = str($item->{self::slugFrom()})->append($suffix)->slug();
+
+		$modelName = get_class($item);
+
+		if (self::isSlugExist($slug, $modelName) && $level < 10) {
+			$slug = self::generateSlug($item, $level+1);
+		}
+
+		return $slug;
+	}
+
+
+	protected static function isSlugExist($slug, $modelName): bool
+	{
+		$object = $modelName::where('slug', $slug)->first();
+		
+		return $object ? true : false;
+	}
+
 
 	public static function slugFrom()
 	{
 		return 'title';
 	}
+
 }
