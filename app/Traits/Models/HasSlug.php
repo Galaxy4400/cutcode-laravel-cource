b737bb87b5e09dd7c16d81920f465eb6d994cd33
Moiseev Evgeny <moiseevEO@yandex.ru>
2023-10-19 17:31:53 +0300

    Slug unicue second try

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -6,12 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasSlug
 {
+	
 	protected static function bootHasSlug(): void
 	{
-		static::creating(function (Model $item) {
-			$item->slug = $item->slug ?? str($item->{self::slugFrom()})->append(time())->slug();
+		static::creating(function (Model $model) {
+			$model->slug = $model->slug ?? $model->generateSlug();
 		});
 	}
+
+
+	protected function generateSlug(int $level = 0): string
+	{
+		$suffix = $level ? '-'.$level : '';
+
+		$slug = str($this->{self::slugFrom()})
+			->append($suffix)
+			->slug();
+
+		if ($this->isSlugExist($slug)) {
+			$slug = $this->generateSlug($level + 1);
+		}
+
+		return $slug;
+	}
+
+
+	protected function isSlugExist(string $slug): bool
+	{
+		$object = $this->where('slug', $slug)->first();
+
+		return $object ? true : false;
+	}
+
 
 	public static function slugFrom()
 	{
