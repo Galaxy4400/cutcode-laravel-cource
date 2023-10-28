987a48ca54e5f4276a71baf1a2b755dc481a5a54
Moiseev Evgeny <moiseevEO@yandex.ru>
2023-10-28 22:28:22 +0300

    commit

--- tests/Feature/App/Http/Controllers/AuthControllerTest.php
+++ tests/Feature/App/Http/Controllers/AuthControllerTest.php
@@ -3,15 +3,16 @@
 namespace Tests\Feature\App\Http\Controllers;
 
 use Tests\TestCase;
+use App\Models\User;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use App\Http\Controllers\AuthController;
-use App\Listeners\SendEmailNewUserListener;
-use App\Models\User;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
 use App\Notifications\NewUserNotification;
+use App\Listeners\SendEmailNewUserListener;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Password;
 
 class AuthControllerTest extends TestCase
 {
@@ -48,6 +49,19 @@ class AuthControllerTest extends TestCase
 			->assertOk()
 			->assertSee('Забыли пароль')
 			->assertViewIs('auth.forgot-password');
+	}
+
+
+	/**
+	 * @return void
+	 */
+	public function test_reset_page_success(): void{
+		$this->get(action([AuthController::class, 'reset'], [
+			'token' => 'test',
+		]))
+			->assertOk()
+			->assertSee('Восстановление паролья')
+			->assertViewIs('auth.reset-password');
 	}
 
 
@@ -138,4 +152,37 @@ class AuthControllerTest extends TestCase
 		$this->assertGuest();
 	}
 
+
+	/**
+	 * @return void
+	 */
+	public function test_reset_password_success(): void
+	{
+		Event::fake();
+
+		$user = User::factory()->create([
+			'password' => bcrypt('old_password'),
+		]);
+
+		$oldPassword = $user->password;
+
+		$token = Password::createToken($user);
+
+		$request = [
+			'email' => $user->email,
+			'password' => '1234567890',
+			'password_confirmation' => '1234567890',
+			'token' => $token,
+		];
+
+		$response = $this->post(action([AuthController::class, 'resetPassword']), $request);
+
+		$newPassword = $user->password;
+
+		$this->assertTrue($oldPassword === $newPassword);
+
+		Event::assertDispatched(PasswordReset::class);
+
+		$response->assertRedirect(route('login'));
+	}
 }
